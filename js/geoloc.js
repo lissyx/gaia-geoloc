252ba12d7fc7586891076e9934a1bde437bf288c
lissyx <lissyx+github@lissyx.dyndns.org>
2012-10-21 20:42:59 +0200

    General improvement
     - Using building blocks
     - Better output of data
     - Enabling and disabling buttons

--- js/geoloc.js
+++ js/geoloc.js
@@ -1,25 +1,29 @@
 var watchId = undefined;
+var sep = "<br />************<br />";
 
 function success(position) {
   var s = document.querySelector('#log');
-  s.innerHTML += "************<br />";
+  s.innerHTML = sep + s.innerHTML;
   for (var i in position.coords) {
     var e = position.coords[i];
     if (typeof e == 'number') {
-      s.innerHTML += i + ":" + position.coords[i] + "<br />";
+      s.innerHTML = i + ":" + position.coords[i] + "<br />" + s.innerHTML;
     }
   }
 }
 
 function error(msg) {
   var s = document.querySelector('#log');
-  s.innerHTML += msg + "<br />";
+  s.innerHTML = sep + msg + sep + s.innerHTML;
 }
 
 function startGeoloc() {
   error("Starting watchPosition");
   if (navigator.geolocation) {
     watchId = navigator.geolocation.watchPosition(success, error);
+    enable('btnStop');
+    enable('btnClear');
+    disable('btnStart');
   } else {
     error('not supported');
   }
@@ -29,6 +33,9 @@ function stopGeoloc() {
   if (watchId != undefined) {
     error("Stopping watchPosition");
     navigator.geolocation.clearWatch(watchId);
+    disable('btnStop');
+    disable('btnClear');
+    enable('btnStart');
   }
 }
 
@@ -36,3 +43,22 @@ function clearGeoloc() {
   var s = document.querySelector('#log');
   s.innerHTML = "";
 }
+
+function enable(id) {
+  var e = document.getElementById(id);
+  if (e) {
+    e.disabled = false;
+  }
+}
+
+function disable(id) {
+  var e = document.getElementById(id);
+  if (e) {
+    e.disabled = true;
+  }
+}
+
+window.onload = function() {
+  disable('btnStop');
+  disable('btnClear');
+};
